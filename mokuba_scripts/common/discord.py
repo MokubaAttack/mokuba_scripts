6a70461dcec8972492a6baaa793adea081baf107
mokuba_attack <ninjapink24@gmail.com>
2026-08-14 22:25:31 +0900

    Add files via upload

--- mokuba_scripts/common/discord.py
+++ mokuba_scripts/common/discord.py
@@ -23,22 +23,24 @@ def to_discord(path,url):
 		f.close()
 	del dbx
 
-def up_drop(path,url):
+def up_drop(path,path2,url):
 	dbx=dropbox.Dropbox(
 		oauth2_refresh_token=url[2],
 		app_key=url[0],
 		app_secret=url[1]
 		)
 	f=open(path, "rb")
-	dbx.files_upload(f.read(), "/"+os.path.basename(path))
+	dbx.files_upload(f.read(), "/"+path2,mode=dropbox.files.WriteMode("overwrite"))
 	f.close()
 	del dbx
 	
-def down_drop(path,url):
+def down_drop(path,path2,url):
 	dbx=dropbox.Dropbox(
 		oauth2_refresh_token=url[2],
 		app_key=url[0],
 		app_secret=url[1]
 		)
-	dbx.files_download_to_file(path, '/'+path)
+	if os.path.exists(path2):
+		os.remove(path2)
+	dbx.files_download_to_file(path2, '/'+path)
 	del dbx
