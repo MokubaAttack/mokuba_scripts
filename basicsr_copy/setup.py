49043116f4fde3fadf8c0fd403889e206bfaf834
mokuba_attack <ninjapink24@gmail.com>
2026-08-22 16:04:12 +0900

    Update setup.py

--- basicsr_copy/setup.py
+++ basicsr_copy/setup.py
@@ -8,6 +8,8 @@ setup(
 	author_email='xintao.wang@outlook.com',
 	keywords='computer vision, restoration, super resolution',
 	url='https://github.com/xinntao/BasicSR',
+	long_description=open('readme.md.md').read(),
+    long_description_content_type='text/markdown',
 	packages=find_packages(exclude=('options', 'datasets', 'experiments', 'results', 'tb_logger', 'wandb')),
 	include_package_data=True,
 	license='Apache License 2.0',
